dc8a22ff7014b55a448873db279ea36cee97d113
LucaRM95 <lucasrojas95@gmail.com>
2021-11-24 21:37:55 -0300

    Se agrega CLI a repositorio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Logo-MI.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Futura página de MI indumentaria en ReactJS
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendé React
         </a>
       </header>
     </div>
