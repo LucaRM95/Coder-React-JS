7cadb1e07af3410ddb45dfca79987565d194220f
LucaRM95 <lucasrojas95@gmail.com>
2021-12-13 14:14:04 -0300

    Se agrega alt a imagenes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -10,7 +10,7 @@ const Item = ({ items }) => {
                 <div key={item.key} className="container">
                     <div className="row">
                         <div className="card col mt-5" style={{width: 18+"rem"}}>
-                            <img className="card-img-top" src={ item.imageURL } alt="Card image cap" />
+                            <img className="card-img-top" src={ item.imageURL } alt={ item.id } />
                             <div className="card-body">
                                 <h5 className="card-title">{ item.title }</h5>
                                 <p className="card-text">${ item.price }</p>
