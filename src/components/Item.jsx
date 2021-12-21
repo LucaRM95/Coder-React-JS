f9ad1130b4dd14d925cfe7ab8fe938e84ef6e122
LucaRM95 <lucasrojas95@gmail.com>
2021-12-21 13:40:21 -0300

    Se agrega Link a imagen de productos

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -9,7 +9,9 @@ const Item = ({ items }) => {
         items.data.map(item => {
             return(
                 <div key={ item.id } className="card col-md-8 mt-5 mb-5 me-2 ms-2" style={{ width: 18+"rem" }}>
-                    <img className="card-img-top" src={ item.url } alt={ item.id } />
+                    <Link to={`/detalles/${name.id}/${item.id}`}>
+                        <img className="card-img-top" src={ item.url } alt={ item.id } />
+                    </Link>
                     <div className="card-body">
                         <h5 className="card-title">{ item.title }</h5>
                         <p className="card-text">Precio: ${ item.price }</p>
